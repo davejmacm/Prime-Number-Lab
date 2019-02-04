e3ef79472bd3651b0e8e49a78aa12499fc84d4ff
davejmacm <davejmacm@gmail.com>
2019-02-04 15:22:45 +0000

    mvp achieved, wanting to display number inputted

---
+++ src/views/result_view.js
@@ -0,0 +1,26 @@
+const PubSub = require('../helpers/pub_sub.js');
+// const FormView = require('../views/form_view.js');
+
+const ResultView = function () {
+
+};
+
+ResultView.prototype.bindEvents = function() {
+  PubSub.subscribe("PrimeChecker:result-checked", (event) => {
+    const primeChecked = event.detail;
+    this.displayResult(primeChecked);
+  });
+
+};
+
+ResultView.prototype.displayResult = function(result) {
+  const resultElement = document.querySelector("#result");
+  if (result === true) {
+    resultElement.textContent = `The number is a prime number!` //${inputtedNumber}
+  }
+  else {
+    resultElement.textContent = `The number is not a prime number, boo!` //${inputtedNumber}
+  }
+};
+
+module.exports = ResultView;
